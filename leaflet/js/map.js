a9733163f481437a61cba5f92e6ce7a4773dfafe
lisalou36 <81833154+lisalou36@users.noreply.github.com>
2021-06-02 10:37:06 -0700

    Update map.js

--- leaflet/js/map.js
+++ leaflet/js/map.js
@@ -36,6 +36,9 @@ function createMap(lat,lon,zl){
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
+	
+	L.Control.geocoder().addTo(map); //add control search geocoder to map
+
 
 	
 }
